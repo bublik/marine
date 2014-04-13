ca763e004483284c25f26513ff72e1a85632dbe1
Ruslan Voloshin <rebisall@gmail.com>
2014-04-13 14:57:40 +0300

    added scope for cert

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -2,4 +2,8 @@ class Cert < ActiveRecord::Base
   validates_presence_of :name
   validates_inclusion_of :category, in: ['medical', 'traning', 'documents']
 
+  scope :medical, -> { where(category: 'medical')}
+  scope :traning, -> { where(category: 'traning')}
+  scope :documents, -> { where(category: 'documents')}
+
 end
