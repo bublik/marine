403bfc8bbfdd4d1c88d7502cab2e6379263d89e3
Ruslan Voloshin <rebisall@gmail.com>
2014-11-21 10:49:19 +0200

    fixed migration

--- db/migrate/20140415202734_create_vessel_engines.rb
+++ db/migrate/20140415202734_create_vessel_engines.rb
@@ -1,65 +1,10 @@
 class CreateVesselEngines < ActiveRecord::Migration
   def change
-    create_table :vessel_engines do |t|
+    create_table :vessel_engines, force: true do |t|
       t.string :name
 
       t.timestamps
     end
-
-    ['B&amp;W',
-     'MAN',
-     'MAK',
-     'Wartsila',
-     'Sulzer',
-     'Mitsubishi',
-     'Pilstick',
-     'Deutz',
-     'Other',
-     'Akasaka',
-     '6NVD',
-     'KOBE DIESEL',
-     'CATERPILLER',
-     'YANMAR',
-     'VOLVO',
-     'STORK',
-     'POTAR',
-     'RD',
-     'GMT',
-     'Hyundai',
-     'Hanshin',
-     'MIRRLEES BLACKSTONE',
-     'Cummins',
-     '8NVD',
-     'Ulstein Aquamaster',
-     'BERGEN',
-     'Ulstein Bergen',
-     'Industrie',
-     'DC Electro Motor',
-     'ABC',
-     'MAN-B&amp;W',
-     'Niigata',
-     'MDDCF-5935846',
-     'KTA 50 M2 (HX)',
-     'CAT-3306',
-     'D12D-C MH',
-     'CAT-3412E',
-     'KTA 19-M4',
-     'C 32',
-     'MWM',
-     'BW-ALPHA',
-     'Blackstone',
-     'Alpha',
-     'BOLNES',
-     'Callesen',
-     'Daihatsu',
-     'Rolls-Royce',
-     'EMD',
-     'CAT',
-     'MACH',
-     'KUMERA',
-     'unknown',
-     'MWM TBD440-06'].each do |name|
-      VesselEngine.create!(name: name)
-    end
+    drop_table :vessel_engines
   end
 end
--- db/migrate/20140519125847_add_vessel_engine_field_to_seaservices.rb
+++ db/migrate/20140519125847_add_vessel_engine_field_to_seaservices.rb
@@ -2,11 +2,9 @@ class AddVesselEngineFieldToSeaservices < ActiveRecord::Migration
   def up
     add_column :seaservices, :vessel_engine, :string
     remove_column :seaservices, :vessel_engine_id
-    drop_table :vessel_engines
   end
 
   def down
-    create_table :vessel_engines
     remove_column :seaservices, :vessel_engine
     add_column :seaservices, :vessel_engine_id, :integer
   end
